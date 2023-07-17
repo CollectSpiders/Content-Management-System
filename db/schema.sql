eab257d43ad5b9a1adcb2aa749f9b657421265ac
CollectSpiders <mathewj519@gmail.com>
2023-07-17 09:08:09 -0700

    finished the table for employees

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,13 @@ USE employee_db;
 -- Creates the employees table
 CREATE TABLE IF NOT EXISTS employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name,
+    role_id,
+    manager_id,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE,
+    SET NULL
 );
 
 -- Creates a table for roles
