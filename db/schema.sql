3014f372333c9bbb60e5302ff69b6fae6f386449
CollectSpiders <mathewj519@gmail.com>
2023-07-17 09:10:50 -0700

    completed the departments table

--- db/schema.sql
+++ db/schema.sql
@@ -16,9 +16,14 @@ CREATE TABLE IF NOT EXISTS employees (
 -- Creates a table for roles
 CREATE TABLE IF NOT EXISTS roles (
     id IN AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department_id(id) ON DELETE CASCADE
 );
 
 -- Creates a table for departments
 CREATE TABLE IF NOT EXISTS departments (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
