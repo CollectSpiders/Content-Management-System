4859053e8468395d2571069f37c5bf9ad530f4d5
CollectSpiders <mathewj519@gmail.com>
2023-07-17 09:21:45 -0700

    added seeds insert for employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,25 @@
-INSERT INTO __ ()
-VALUES (""),
-       (""),
-       (""),
-       (""),
-       ("");
+-- general, manager, engineer, system admin, chief director
 
-INSERT INTO __ ()
-VALUES (),
-       (),
-       (),
-       (),
-       (),
-       (),
-       ();
-       
+-- Inserts employees
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('john', 'doe', 1, null);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('jane','doe',2,null);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('kyle','king',3,null);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('kayla','kules',4,null);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('scott','sephs',5,null);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('adrian','lee',6,null);
+
+-- inserts roles
+
+
+-- inserts departments
