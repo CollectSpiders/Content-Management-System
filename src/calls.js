77e6109470fc329fe3d12a026f8d5db081e128c7
CollectSpiders <mathewj519@gmail.com>
2023-07-18 10:42:50 -0700

    added the add employee call

--- src/calls.js
+++ src/calls.js
@@ -6,3 +6,47 @@ const {
     updateEmployeeRolePrompt,
     updateEmployeeManagerPrompt
 } = require('./prompt');
+
+const {
+    getEmployees,
+    getRoles,
+    getManagers,
+    getDepartments,
+    createEmployee,
+    createRole,
+    createDepartment,
+    deleteObject,
+    updateEmployeeRole,
+    updateEmployeeManager
+} = require('./operations');
+
+const viewAllEmployees = async () =>{
+    const employees = await getEmployees();
+    console.table(employees);
+};
+
+const viewAllRoles = async () =>{
+    const roles = await getRoles();
+    console.table(roles);
+};
+
+const viewAllDepartments = async () =>{
+    const departmens = await getDepartments();
+    console.table(departments);
+};
+
+const add Employee = async () =>{
+    const {
+        employeeFirtName,
+        employeeLastName,
+        employeeRole,
+        employeeManager
+    } = await addEmployeePrompt();
+    await createEmployee(
+        employeeFirtName,
+        employeeLastName,
+        employeeRole,
+        employeeManager
+    );
+    console.log(`\n ${employeeFirtName} ${employeeLastName} has been added to the database`);
+};
